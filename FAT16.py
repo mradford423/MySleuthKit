953079871ee449002dc7f28e57d9ff03766d91c6
mradford423 <mradford@umass.edu>
2015-03-12 11:26:50 -0400

    added FAT16.py

---
+++ FAT16.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python3
+import sys
+import struct
+
+#Written by Michael Radford
+
+def usage():
+	'''
+	Returns an error if run without being given a FAT16 image
+	'''
+	print("Error:")
+	print("USAGE: hw3.py image")
+	print("exiting...")
+	sys.exit()
+
+def parse(fd):
+	'''
+	parses the image and prints the results
+	'''
+
+	'''
+	Check for FAT16
+	'''
+	fd.seek(54)
+	bytes = fd.read(8)
+	fsys = chr(bytes[0]) + chr(bytes[1]) + chr(bytes[2]) + chr(bytes[3]) + chr(bytes[4])
+	if(fsys != "FAT16"):
+		usage()
+	print("FILE SYSTEM INFORMATION")
+	print("--------------------------------------------")
+	print("File System Type:", fsys)
+	
+	'''
+	Find OEM Name
+	'''
+	fd.seek(3)
+	bytes = fd.read(8)
+	print("")
+	print("OEM Name: ", end="")
+	for b in bytes:
+		print(chr(b), end="")
+	print("")
+
+	'''
+	Find Volume ID and Label
+	'''
+	fd.seek(39)
+	bytes = fd.read(4)
+	print('Volume ID: 0x', end="")
+	for b in reversed(bytes):
+		print("%02x" % b, end="")
+	print("")
+	bytes = fd.read(11)
+	print('Volume Label (Boot Sector): ', end="")
+	for b in bytes:
+		print (chr(b), end="")
+	print("")
+	print("")
+	
+	'''
+	Already found File System Type Label
+	'''
+	print("File System Type Label:", fsys)
+	print("")
+
+	'''
+	Prints the layout on the File System in sectors
+	'''
+	print("File System Layout (in sectors)")
+	
+	#Total range
+	fd.seek(19)
+	bytes = fd.read(2)
+	sectors = (bytes[1]<<8) + (bytes[0]) - 1
+	if(sectors == 0):
+		fd.seek(32)
+		bytes = fd.read(4)
+		sectors = (bytes[3]<<24) + (bytes[2]<<16) + (bytes[1]<<8) + (bytes[0]) - 1
+	print("Total Range: 0 -", sectors)
+	print("Total Range in Image: 0 -", sectors - 1)
+	
+	#Reserved Space
+	fd.seek(14)
+	bytes = fd.read(2)
+	reserved = (bytes[1]<<8) + (bytes[0]) - 1
+	print("* Reserved: 0 -", reserved)
+	
+	#Boot Sector (Assuming always 0)
+	print("** Boot Sector: 0")
+	
+	#Location of FAT(s)
+	fd.seek(22)
+	bytes = fd.read(2)
+	fatsize = (bytes[1]<<8) + (bytes[0])
+	print("* FAT 0: ", reserved + 1, "-", fatsize + reserved )
+	fd.seek(16)
+	if(fd.read()[0] == 2):
+		print("* FAT 1: ", fatsize + reserved + 1, "-", fatsize + fatsize + reserved)
+	
+	#Data Area
+	datastart = fatsize + fatsize + reserved + 1
+	print("* Data Area: ", datastart, "-", sectors)
+	
+	#Size of each Sector (used now, not printed until later)
+	fd.seek(11)
+	bytes = fd.read(2)
+	secsize = (bytes[1]<<8) + (bytes[0])
+	
+	# number of sectors per cluster (used for calculations, not printed)
+	fd.seek(13)
+	sec_per_clus = fd.read(1)[0]
+	
+	#Root Directory
+	fd.seek(17)
+	bytes = fd.read(2)
+	rootsize = (bytes[1]<<8) + (bytes[0])
+	print("** Root Directory: ", datastart, "-", datastart + int((rootsize*32)/512) - 1)
+	
+	#Cluster Area
+	clus_end = sectors - ((sectors-1)%2)
+	print("** Cluster Area: ", datastart + int((rootsize*32)/512), "-", clus_end)
+	
+	#Non-Clustered Area
+	print("** Non-clustered: ", clus_end + 1, "-", sectors)
+	print("")
+	
+	'''
+	Final content info 
+	'''
+	print("CONTENT INFORMATION")
+	print("--------------------------------------------")
+	print("Sector Size: ", secsize, "bytes")
+	print("Cluster Size: ", sec_per_clus * secsize, "bytes")
+	#Total Cluster range uses an equation found on page 164 of carrier
+	print("Total Cluster Range:  2 -", int(((clus_end - (datastart + (rootsize*32)/512))/sec_per_clus) + 2))
+	print("")
+
+
+
+def main():
+	'''
+	Tries to parse the given FAT16 image
+	Calls usage() if there is an error, calls parse() otherwise
+	'''
+	try:
+		image = sys.argv[1]
+		fd = open(image, "rb")
+	except:
+		usage()
+	parse(fd)
+
+
+if __name__ == '__main__':
+	main()
