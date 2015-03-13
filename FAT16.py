3bc5295c477507893cf9c444bfebb13210006a62
mradford423 <mradford@umass.edu>
2015-03-12 21:23:44 -0400

    added offset functionality to FAT16

--- FAT16.py
+++ FAT16.py
@@ -6,34 +6,33 @@ import struct
 
 def usage():
 	'''
-	Returns an error if run without being given a FAT16 image
+	Returns an error if run without being given an offset and a FAT16 image
 	'''
 	print("Error:")
-	print("USAGE: hw3.py image")
+	print("USAGE: hw3.py offset(in sectors) image")
 	print("exiting...")
 	sys.exit()
 
-def parse(fd):
+def parse(offset, fd):
 	'''
 	parses the image and prints the results
 	'''
 
 	'''
-	Check for FAT16
+	Check signature for FAT16
 	'''
-	fd.seek(54)
-	bytes = fd.read(8)
-	fsys = chr(bytes[0]) + chr(bytes[1]) + chr(bytes[2]) + chr(bytes[3]) + chr(bytes[4])
-	if(fsys != "FAT16"):
+	fd.seek(510+(offset*512))
+	bytes = fd.read(2)
+	if(bytes != b'\x55\xAA'):
 		usage()
 	print("FILE SYSTEM INFORMATION")
 	print("--------------------------------------------")
-	print("File System Type:", fsys)
+	print("File System Type: FAT16")
 	
 	'''
 	Find OEM Name
 	'''
-	fd.seek(3)
+	fd.seek(3+(offset*512))
 	bytes = fd.read(8)
 	print("")
 	print("OEM Name: ", end="")
@@ -44,7 +43,7 @@ def parse(fd):
 	'''
 	Find Volume ID and Label
 	'''
-	fd.seek(39)
+	fd.seek(39+(offset*512))
 	bytes = fd.read(4)
 	print('Volume ID: 0x', end="")
 	for b in reversed(bytes):
@@ -60,7 +59,7 @@ def parse(fd):
 	'''
 	Already found File System Type Label
 	'''
-	print("File System Type Label:", fsys)
+	print("File System Type Label: FAT16")
 	print("")
 
 	'''
@@ -69,18 +68,18 @@ def parse(fd):
 	print("File System Layout (in sectors)")
 	
 	#Total range
-	fd.seek(19)
+	fd.seek(19+(offset*512))
 	bytes = fd.read(2)
 	sectors = (bytes[1]<<8) + (bytes[0]) - 1
 	if(sectors == 0):
-		fd.seek(32)
+		fd.seek(32+(offset*512))
 		bytes = fd.read(4)
 		sectors = (bytes[3]<<24) + (bytes[2]<<16) + (bytes[1]<<8) + (bytes[0]) - 1
 	print("Total Range: 0 -", sectors)
 	print("Total Range in Image: 0 -", sectors - 1)
 	
 	#Reserved Space
-	fd.seek(14)
+	fd.seek(14+(offset*512))
 	bytes = fd.read(2)
 	reserved = (bytes[1]<<8) + (bytes[0]) - 1
 	print("* Reserved: 0 -", reserved)
@@ -89,11 +88,11 @@ def parse(fd):
 	print("** Boot Sector: 0")
 	
 	#Location of FAT(s)
-	fd.seek(22)
+	fd.seek(22+(offset*512))
 	bytes = fd.read(2)
 	fatsize = (bytes[1]<<8) + (bytes[0])
 	print("* FAT 0: ", reserved + 1, "-", fatsize + reserved )
-	fd.seek(16)
+	fd.seek(16+(offset*512))
 	if(fd.read()[0] == 2):
 		print("* FAT 1: ", fatsize + reserved + 1, "-", fatsize + fatsize + reserved)
 	
@@ -102,16 +101,16 @@ def parse(fd):
 	print("* Data Area: ", datastart, "-", sectors)
 	
 	#Size of each Sector (used now, not printed until later)
-	fd.seek(11)
+	fd.seek(11+(offset*512))
 	bytes = fd.read(2)
 	secsize = (bytes[1]<<8) + (bytes[0])
 	
 	# number of sectors per cluster (used for calculations, not printed)
-	fd.seek(13)
+	fd.seek(13+(offset*512))
 	sec_per_clus = fd.read(1)[0]
 	
 	#Root Directory
-	fd.seek(17)
+	fd.seek(17+(offset*512))
 	bytes = fd.read(2)
 	rootsize = (bytes[1]<<8) + (bytes[0])
 	print("** Root Directory: ", datastart, "-", datastart + int((rootsize*32)/512) - 1)
@@ -143,11 +142,12 @@ def main():
 	Calls usage() if there is an error, calls parse() otherwise
 	'''
 	try:
-		image = sys.argv[1]
+		offset = int(sys.argv[1])
+		image = sys.argv[2]
 		fd = open(image, "rb")
 	except:
 		usage()
-	parse(fd)
+	parse(offset,fd)
 
 
 if __name__ == '__main__':
